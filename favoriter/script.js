052da63bf2f05dcaa70e84e71a99724bac7ca762
Alvin Berglund <albe0411@storsjogymnasiet.se>
2024-03-28 09:56:01 +0100

    favvosida 1.5/2.5

--- favoriter/script.js
+++ favoriter/script.js
@@ -1,37 +1,25 @@
-function bytatext(num1, num2, num3, button1, button2, button3) {
-    let show = document.getElementById(num1);
-    let hide1 = document.getElementById(num2);
-    let hide2 = document.getElementById(num3);
-    let standard = document.getElementById("standard");
+let standardDiv = document.getElementById("standard")
+let filmerDiv = document.getElementById("filmer")
+let serierDiv = document.getElementById("serier")
+let låtarDiv = document.getElementById("låtar")
 
-    let shadowd = document.getElementById(button1);
-    let normal1 = document.getElementById(button2);
-    let normal2 = document.getElementById(button3);
-
-    if (show.style.display === "block") {
-        show.style.display = "none";
+function bytatext(sidan) {
+    if (sidan == "filmer") {
+        standardDiv.style.display = "none"
+        filmerDiv.style.display = "block"
+        serierDiv.style.display = "none"
+        låtarDiv.style.display = "none"
     }
-    else {
-        show.style.display = "block";
+    else if (sidan == "serier") {
+        standardDiv.style.display = "none"
+        filmerDiv.style.display = "none"
+        serierDiv.style.display = "block"
+        låtarDiv.style.display = "none"
     }
-
-    hide1.style.display = "none";
-    hide2.style.display = "none";
-
-    if (show.style.display === "none" & hide1.style.display === "none" & hide2.style.display === "none") {
-        standard.style.display = "block";
+    else if (sidan == "låtar") {
+        standardDiv.style.display = "none"
+        filmerDiv.style.display = "none"
+        serierDiv.style.display = "none"
+        låtarDiv.style.display = "block"
     }
-    else {
-        standard.style.display = "none";
-    }
-
-    if (shadowd.style.filter == "brightness(100%)") {
-        shadowd.style.filter = "brightness(80%)"
-    }
-    else {
-        shadowd.style.filter = "brightness(100%)"
-    }
-
-    normal1.style.filter = "brightness(100%)"
-    normal2.style.filter = "brightness(100%)"
 }
