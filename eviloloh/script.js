e1b6e7859487f8fbc77c53dfa73d1c47bdd2f45b
Alvin Berglund <albe0411@storsjogymnasiet.se>
2024-06-05 11:25:13 +0200

    eviloloh 4+/4(5?) done with untalent main (+ small fixes)

--- eviloloh/script.js
+++ eviloloh/script.js
@@ -1,6 +1,6 @@
 let navList = document.getElementById("nav_list");
 let header = document.getElementById("header_id");
-let width = screen.availWidth;
+let navBar = document.getElementById("nav_bar");
 
 function burgirMenu(){
     if (navList.classList == "nav_list"){
@@ -8,11 +8,14 @@ function burgirMenu(){
         navList.classList.remove("nav_list")
         header.classList.add("header_when_burgir")
         header.classList.remove("header")
+        navBar.style.display = "flex"
     }
+
     else {
         navList.classList.add("nav_list")
         navList.classList.remove("nav_burgir_menu")
         header.classList.remove("header_when_burgir")
         header.classList.add("header")
+        navBar.style.display = "none"
     }
 }
